f6c424825a010fa973037044d7c716cc95ed776c
theo-michel <theo64200@gmail.com>
2023-07-26 14:18:48 +0900

    update to the website

--- run.sh
+++ run.sh
@@ -2,3 +2,11 @@
 npm run build:html
 npm run build:tailwind
 start "" "public\index.html"
+
+
+
+#   "scripts": {
+#     "build:html": "node yaml2json.js && ./build.sh",
+#     "build:tailwind": "NODE_ENV=production node_modules/.bin/postcss scss/style.scss -o public/css/style.css",
+#     "watch:tailwind": "node_modules/.bin/postcss scss/style.scss -o public/css/style.css --watch"
+#   },
