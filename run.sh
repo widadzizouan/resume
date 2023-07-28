ca6fcd4e88650d82b8d9a7ae01b7102ce41867b6
theo-michel <theo64200@gmail.com>
2023-07-28 16:59:06 +0100

    corrected logo and / and oppurtunity ..

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 # npm install
 npm run build:html
+ls
 npm run build:tailwind
 start "" "public\index.html"
 
@@ -8,5 +9,12 @@ start "" "public\index.html"
 #   "scripts": {
 #     "build:html": "node yaml2json.js && ./build.sh",
 #     "build:tailwind": "NODE_ENV=production node_modules/.bin/postcss scss/style.scss -o public/css/style.css",
+#     "watch:tailwind": "node_modules/.bin/postcss scss/style.scss -o public/css/style.css --watch"
+#   },
+
+# WINDOWS
+#   "scripts": {
+#     "build:html": "node yaml2json.js && bash build.sh",
+#     "build:tailwind": "node_modules/.bin/postcss scss/style.scss -o public/css/style.css",
 #     "watch:tailwind": "node_modules/.bin/postcss scss/style.scss -o public/css/style.css --watch"
 #   },
